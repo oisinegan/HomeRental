fba5ffe8b3957cd36d00cf8566299206d233a08a
oisinegan <oisinegan00@gmail.com>
2023-07-26 14:29:41 +0100

    Upload multiple images to storage

--- Client/front-end-home-rental/src/pages/PostAd.js
+++ Client/front-end-home-rental/src/pages/PostAd.js
@@ -1,13 +1,17 @@
 import Nav from "../components/nav";
 import React, { useState, useEffect } from "react";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import storage from "../config/firebase";
 
 function PostAd() {
-  const [image, setImage] = useState(null);
+  const [images, setImages] = useState(null);
   const [imagePre, setImagePre] = useState(null);
-  const [file, setFile] = useState();
-  const [imagePreview, setImagePreview] = useState();
+
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [uploadedFilesImgURl, setUploadedFilesImgUrl] = useState([]);
+  const [fileLimit, setFileLimit] = useState(false);
+  const MAX_IMAGES = 4;
+
   const [userInfo, setUserInfo] = useState([{}]);
   const [formRes, setFormRes] = useState({
     Type: "",
@@ -17,7 +21,10 @@ function PostAd() {
     Price: "0",
     Bedrooms: "0",
     Bathrooms: "0",
-    UID: "",
+    idLandlord: "",
+    url: "",
+    DatePosted: "",
+    Folder: "",
   });
 
   const handleChange = (e) => {
@@ -31,31 +38,6 @@ function PostAd() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    let formData = new FormData();
-    formData.append("Type", formRes.Type);
-    formData.append("City", formRes.City);
-    formData.append("County", formRes.County);
-    formData.append("Address", formRes.Address);
-    formData.append("Price", formRes.Price);
-    formData.append("Bedrooms", formRes.Bedrooms);
-    formData.append("Bathrooms", formRes.Bathrooms);
-    formData.append("UID", formRes.UID);
-
-    for (const pair of formData.entries()) {
-      console.log(pair[0], pair[1]);
-    }
-
-    const response = await fetch("/PostAd", {
-      method: "post",
-      body: formData,
-    });
-
-    const result = await response.json();
-    alert("MSG FROM BACKEND " + result);
-    console.log(result);
-  };
-
-  const handleChangeImage = (e) => {
     let date_ob = new Date();
     let date = ("0" + date_ob.getDate()).slice(-2);
     let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
@@ -79,20 +61,82 @@ function PostAd() {
       month +
       "-" +
       year;
-    console.log(fullDate);
 
-    const folder = formRes.UID + "_" + fullDate;
+    const folder = formRes.idLandlord + "_" + fullDate;
+    formRes.Folder = folder;
+    formRes.DatePosted = date + "-" + month + "-" + year;
 
-    console.log(folder);
-    const imageRef = ref(storage, folder);
+    console.log(uploadedFiles);
+    const promise = [];
+    uploadedFiles.map(async (img) => {
+      const imageRef = ref(storage, folder + "/" + img.name);
+      await uploadBytes(imageRef, img);
+      const imageUrl = await getDownloadURL(imageRef);
+      console.log(imageUrl);
+      //formRes.url = imageUrl
+    });
+    // const imageRef = ref(storage, folder + "/" + images.name);
 
-    setImage(e.target.files[0]);
-    setImagePre(URL.createObjectURL(image));
-    console.log(image);
+    //
+    // const imageUrl = await getDownloadURL(imageRef);
+    // formRes.url = imageUrl;
+    // await sendForm();
+  };
 
-    // uploadBytes(imageRef, image).then((snapshot) => {
-    //   console.log("Uploaded a blob or file!");
-    // });
+  const sendForm = async () => {
+    const response = await fetch("/PostAd", {
+      method: "post",
+      body: JSON.stringify(formRes),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    alert("MSG FROM BACKEND " + result);
+  };
+
+  // const handleChangeImage = (e) => {
+  //   // setImages(e.target.files[0]);
+  //   // const urlVal = URL.createObjectURL(e.target.files[0]);
+  //   // setImagePre(urlVal);
+
+  //   console.log(e.target.files);
+  // };
+  //CREATE OBJECT URLs
+  const configImages = async (files) => {
+    const urls = [];
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const objectURL = URL.createObjectURL(file);
+      urls.push(objectURL);
+    }
+    setUploadedFilesImgUrl(urls);
+    console.log(uploadedFilesImgURl);
+    console.log(urls);
+  };
+  const handleUploadFiles = (files) => {
+    const uploaded = [...uploadedFiles];
+    let limitExceeded = false;
+
+    files.some((file) => {
+      if (uploaded.findIndex((f) => f.name === file.name) === -1) {
+        uploaded.push(file);
+        if (uploaded.length === MAX_IMAGES) {
+          alert("YOU CAN ONLY ADD " + (MAX_IMAGES - 1) + " images");
+          setFileLimit(false);
+          limitExceeded = true;
+          return true;
+        }
+      }
+    });
+    if (!limitExceeded) {
+      setUploadedFiles(uploaded);
+      configImages(uploaded);
+    }
+  };
+  const handleFileEvent = (e) => {
+    const chosenFiles = Array.prototype.slice.call(e.target.files);
+    handleUploadFiles(chosenFiles);
   };
 
   useEffect(() => {
@@ -100,9 +144,10 @@ function PostAd() {
       .then((res) => res.json())
       .then((userInfo) => {
         setUserInfo(userInfo);
+        console.log(userInfo);
         setFormRes((prevFormRes) => ({
           ...prevFormRes,
-          UID: userInfo.id,
+          idLandlord: userInfo.id,
         }));
       });
   }, []);
@@ -137,8 +182,22 @@ function PostAd() {
         </label>
         <br /> <br />
         <label>Add Image:</label>
-        <input name="file" type="file" required onChange={handleChangeImage} />
-        <img height={150} width={150} src={imagePre} />
+        <input
+          name="file"
+          type="file"
+          required
+          onChange={handleFileEvent}
+          multiple
+          disabled={fileLimit}
+        />
+        {/* <img height={150} width={150} src={imagePre} /> */}
+        <div className="uploaded-files-list">
+          {uploadedFilesImgURl.map((file) => (
+            <div>
+              <img height={150} width={150} src={file} />
+            </div>
+          ))}
+        </div>
         <br /> <br />
         <br /> <br />
         <label>Address</label>
@@ -184,36 +243,6 @@ function PostAd() {
         <br /> <br />
         <button type="submit">Submit</button>
       </form>
-      <div>
-        {typeof searchRes === "undefined" ? (
-          <p>loading....</p>
-        ) : (
-          <div>
-            {/* {searchRes.map((house, index) => (
-              <div key={index}>
-                <div>
-                  <img src={house.url} width={100} height={100}></img>
-                  {house.idHome +
-                    "," +
-                    house.Type +
-                    "," +
-                    house.Address +
-                    "," +
-                    house.City +
-                    "," +
-                    house.County +
-                    "," +
-                    house.Price +
-                    "," +
-                    house.Bedrooms +
-                    "," +
-                    house.Bathrooms}
-                </div>
-              </div>
-            ))} */}
-          </div>
-        )}
-      </div>
     </>
   );
 }
--- Server/routes/PostAd.js
+++ Server/routes/PostAd.js
@@ -1,128 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../config/dbConfig");
-//const { getStorage } = require("firebase-admin/storage");
-const { uploadBytes } = require("firebase/storage");
-const { ref } = require("firebase/storage");
-const { initializeApp } = require("firebase-admin/app");
-// const getStorage = require("../node_modules/firebase/storage");
-const { getStorage } = require("firebase-admin/storage");
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCe_gt78ziwC_Q3wjIU0eVOcZBNX49q1Zc",
-//   authDomain: "houserentalimages.firebaseapp.com",
-//   projectId: "houserentalimages",
-//   storageBucket: "houserentalimages.appspot.com",
-//   messagingSenderId: "872853039263",
-//   appId: "1:872853039263:web:acc0bc3eed092d069a1aaa",
-//   measurementId: "G-QXTW4JWRNV",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const storage = getStorage(app);
-
-//--------------------------------------------------------------------------------------------
-const admin = require("firebase-admin");
-const serviceAccount = require("../houserentalimages-firebase-adminsdk-jyge2-eaaf09321a.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: "houserentalimages.appspot.com",
-});
-
-const bucket = admin.storage().bucket();
-
-//--------------------------------------------------------------------------------------------
-async function uploadFile(filePath, destinationPath) {
-  try {
-    await bucket.upload(filePath, {
-      destination: destinationPath,
-    });
-    console.log("File uploaded successfully.");
-  } catch (error) {
-    console.error("Error uploading file:", error);
-  }
-}
 
 router.post("/", (req, res) => {
   connection.connect();
 
   let info = req.body;
 
-  console.log(info);
+  const sql =
+    "INSERT INTO `HomeRental`.`Home` (`Type`, `Address`, `City`, `County`, `Price`, `Bedrooms`, `Bathrooms`, `url`, `idLandlord`, `DatePosted`,`Folder`) VALUES ('" +
+    info.Type +
+    "', '" +
+    info.Address +
+    "', '" +
+    info.City +
+    "', '" +
+    info.County +
+    "', '" +
+    info.Price +
+    "', '" +
+    info.Bedrooms +
+    "', '" +
+    info.Bathrooms +
+    "', '" +
+    info.url +
+    "', '" +
+    info.idLandlord +
+    "', '" +
+    info.DatePosted +
+    "', '" +
+    info.Folder +
+    "');";
 
-  let date_ob = new Date();
-  let date = ("0" + date_ob.getDate()).slice(-2);
-  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-  let year = date_ob.getFullYear();
-  let hours = date_ob.getHours();
-  let minutes = date_ob.getMinutes();
-  let seconds = date_ob.getSeconds();
-  let milli = date_ob.getMilliseconds();
-
-  let fullDate =
-    hours +
-    ":" +
-    minutes +
-    ":" +
-    seconds +
-    ":" +
-    milli +
-    "/" +
-    date +
-    "-" +
-    month +
-    "-" +
-    year;
-  console.log(fullDate);
-
-
-
-  // const imageRef = ref(storage, folder);
-  // uploadBytes(imageRef, info.file).then(() => {
-  //   console.log("IMAGE UPLOADED");
-  // });
-
-  // const imageRef = ref(storage, fullDate);
-
-  //IMage the folder is user id + full date
-
-  //USERID. - Ad1/Ad2/Ad3
-
-  // if (info.file === "") {
-  //   res.send('"ERROR RECIEVING IMAGE"');
-  // }else{
-  //   const imageRef = ref(storage,)
-  // }
-
-  // const sql =
-  //   "SELECT * FROM HomeRental.Home WHERE Price BETWEEN " +
-  //   info.MinPrice +
-  //   " AND " +
-  //   info.MaxPrice +
-  //   " AND Bedrooms BETWEEN " +
-  //   info.MinBedrooms +
-  //   " AND " +
-  //   info.MaxBedrooms +
-  //   " AND Bathrooms BETWEEN " +
-  //   info.MinBathrooms +
-  //   " AND " +
-  //   info.MaxBathrooms +
-  //   " AND City LIKE '" +
-  //   info.City +
-  //   "%'" +
-  //   " AND County LIKE '" +
-  //   info.County +
-  //   "%'" +
-  //   " AND Type LIKE '" +
-  //   info.Type +
-  //   "%'";
-  // connection.query(sql, (err, rows, fields) => {
-  //   if (err) throw err;
-
-  //   res.send(rows);
-  //  });
+  connection.query(sql, (err, rows, fields) => {
+    if (err) throw err;
+    res.send('"RECIEVED"');
+  });
 });
 
 module.exports = router;
